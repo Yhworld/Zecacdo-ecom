a7b100a56258d45e6dd7a5de5ee58b78266e8bda
Yahya <yahyahussein670@gmail.com>
2024-04-20 23:32:31 +0300

    update: install various dependacies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightGreen: 'hsl(157, 49%, 49%)',
+        btbBrown: '#E6D8BD'
+
+      },
+    },
   },
   plugins: [],
 }
-
