5f7f68bb72796d36ed96c4b0c9e6c5b26e450248
Yahya <yahyahussein670@gmail.com>
2024-05-05 09:52:12 +0300

    create: a fake data to be displayed in the featured component

---
+++ src/assets/data.js
@@ -0,0 +1,31 @@
+import image1 from './imagebg4.png'
+import image2 from './imagebg4.png'
+
+
+let dataProduct = [
+    {
+      "id": 1,
+      "title": "Zecado Enigma",
+      "price": 50,
+      "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      "category": "men's clothing",
+      "image": image1
+    },
+    {
+      "id": 2,
+      "title": "Zecado Enigma",
+      "price": 22,
+      "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+      "category": "men's clothing",
+      "image": image2,
+    },
+    {
+      "id": 3,
+      "title": "Zecado Enigma",
+      "price": 55.99,
+      "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+      "category": "men's clothing",
+      "image": image2,
+    }
+  ]
+  export default dataProduct
