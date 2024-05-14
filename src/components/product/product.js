dcafc56d971fa059b3aa380591d99475bc2ffd8c
Yahya <yahyahussein670@gmail.com>
2024-05-14 18:03:34 +0300

    create: layout and structure of the product component where the product is displayed in details

---
+++ src/components/product/product.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { ProductDisplay } from '../productDisplay/ProductDisplay';
+
+const ProductDetail = () => {
+  const { id } = useParams(); // Get the product id from URL params
+  const product = useSelector(state => state.products.products.find(p => p.id === parseInt(id)));
+
+  if (!product) {
+    return <div>Product not found</div>;
+  }
+
+  return (
+    <div>
+      <ProductDisplay product={product}/>
+    </div>
+  );
+};
+
+export default ProductDetail;
