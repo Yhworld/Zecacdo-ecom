8d48d30caff2b60a11430f3f441ddc5da1c0cf34
Yahya <yahyahussein670@gmail.com>
2024-05-19 23:24:32 +0300

    add a cart reducer functionality which includes adding and removing items from carts

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../slices/ProductSlice";
+import { cartReducer } from "../slices/CartSlice";
 
 export const store = configureStore({
-    reducer: {
-        products: productReducer
-    }
-})
+  reducer: {
+    products: productReducer,
+    cart: cartReducer,
+  },
+});
