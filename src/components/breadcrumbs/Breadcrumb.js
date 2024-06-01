58079bd132191bd7b68fa3722acf70162d4b0301
Yahya <yahyahussein670@gmail.com>
2024-06-01 12:32:24 +0300

    create: breadcrumbs for easy navigation around pages

---
+++ src/components/breadcrumbs/Breadcrumb.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './breadcrumb.css';
+
+const Breadcrumb = ({ pathMapping = {}, extraBreadcrumbs = [] }) => {
+  const location = useLocation();
+  const paths = location.pathname.split('/').filter((path) => path);
+
+  const breadcrumbLinks = paths.map((path, index) => {
+    const to = `/${paths.slice(0, index + 1).join('/')}`;
+    const isLast = index === paths.length - 1;
+    const displayName = pathMapping[to] || path.charAt(0).toUpperCase() + path.slice(1);
+
+    return (
+      <li key={to} className={`breadcrumb-item ${isLast ? 'active' : ''}`}>
+        {isLast ? (
+          displayName
+        ) : (
+          <Link to={to}>{displayName}</Link>
+        )}
+      </li>
+    );
+  });
+
+  return (
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb mx-auto container max-w-screen-xl">
+        <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+        {extraBreadcrumbs.map(({ name, path }, index) => (
+          <li key={path} className={`breadcrumb-item ${index === extraBreadcrumbs.length - 1 ? 'active' : ''}`}>
+            {index === extraBreadcrumbs.length - 1 ? (
+              name
+            ) : (
+              <Link to={path}>{name}</Link>
+            )}
+          </li>
+        ))}
+        {!extraBreadcrumbs.length && breadcrumbLinks}
+      </ol>
+    </nav>
+  );
+};
+
+export default Breadcrumb;
