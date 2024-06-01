0174a4a89a71e6f8624192564fdc8f704fdefb0e
Yahya <yahyahussein670@gmail.com>
2024-06-01 12:29:51 +0300

    update: wrap the persist element around the app

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { persistor, store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
   <BrowserRouter>
+  <PersistGate persistor={persistor}>
     <App />
+  </PersistGate>
   </BrowserRouter>
   </Provider>
 );
