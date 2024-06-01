deb6ca9c29f249a538d582c657df6b3cccbf6e84
Yahya <yahyahussein670@gmail.com>
2024-06-01 12:42:16 +0300

    create: add loading functionality and import breadcrumbs

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import ProductDisplay from '../productDisplay/ProductDisplay'; // Ensure correct import path
+import Breadcrumb from '../breadcrumbs/Breadcrumb';
+import { fetchProductById } from '../../slices/ProductSlice'; // Ensure correct import path
 
 const ProductDetail = () => {
-  const { id } = useParams(); // Get the product id from URL params
-  const product = useSelector(state => state.products.products.find(p => p.id === id));
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const product = useSelector(state => state.products.selectedProduct);
+  const loading = useSelector(state => state.products.loading);
+  const error = useSelector(state => state.products.error);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchProductById(id));
+    }
+  }, [dispatch, id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   if (!product) {
     return <div>Product not found</div>;
   }
 
+  const pathMapping = {
+    '/shop': 'Shop',
+    [`/product/${id}`]: product.name,
+  };
+
+  const extraBreadcrumbs = [
+    { name: 'Shop', path: '/shop' },
+    { name: product.name, path: `/product/${id}` }
+  ];
+
   return (
     <div>
+      <Breadcrumb pathMapping={pathMapping} extraBreadcrumbs={extraBreadcrumbs} />
       <ProductDisplay product={product} />
     </div>
   );
