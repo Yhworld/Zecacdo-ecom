3c71d533209e5e81a255e9ad4ac371a29367844f
Yahya <yahyahussein670@gmail.com>
2024-06-01 12:38:38 +0300

    update: position and styling

--- src/components/cartItem/CartItem.js
+++ src/components/cartItem/CartItem.js
@@ -1,40 +1,47 @@
 import React from "react";
-import {
-  incrementQuantity,
-  decrementQuantity,
-  removeItem,
-} from "../../slices/CartSlice";
+import { incrementQuantity, decrementQuantity, removeItem } from "../../slices/CartSlice";
 import { useDispatch } from "react-redux";
 
 function CartItem({ id, image, title, price, quantity }) {
   const dispatch = useDispatch();
 
   return (
-    <div className="container mx-auto">
-      <div className="flex md:space-x-24 items-center ">
-        <div>
-          <div>product</div>
-          <img className="w-32" src={image} alt="item" />
+    <div className="container mx-auto border md:border-gray-200 md:p-4 mb-4">
+      <div className="flex items-center justify-between">
+        <div className="w-1/4">
+          <div className="text-gray-700 mb-2">Product</div>
+          <img className="w-32 h-32 object-cover" src={image} alt="item" />
         </div>
-          <p className="flex items-center">{title}</p>
-
-            <div className="flex space-x-4 border rounded-md border-slate-800 py-2 px-4">
-              <button onClick={() => dispatch(decrementQuantity(id))}>-</button>
-              <p className="">{quantity}</p>
-              <button onClick={() => dispatch(incrementQuantity(id))}>+</button>
-          </div>
-          <div className="flex flex-col space-y-4">
-            <div className="">price</div>
-            <div className="flex items-center font-medium">$ {price}</div>
-            <div className="">
-            <button
-              className=""
-              onClick={() => dispatch(removeItem(id))}
-            >
-              Remove
-            </button>
-            </div>
-          </div>
+        <div className="w-1/4">
+          <p className="text-gray-700 text-lg">{title}</p>
+        </div>
+        <div className="flex flex-col items-center mb-6">
+        <div className="text-gray-700 mb-2">Qty</div>
+        <div className="flex items-center justify-center md:border border-gray-400 px-2 py-2 rounded-md space-x-4">
+          <button
+            className=""
+            onClick={() => dispatch(decrementQuantity(id))}
+          >
+            -
+          </button>
+          <p className="mx-2">{quantity}</p>
+          <button
+            className=""
+            onClick={() => dispatch(incrementQuantity(id))}
+          >
+            +
+          </button>
+        </div></div>
+        <div className="w-1/4 flex flex-col items-end">
+          <div className="text-gray-700 mb-2">Price</div>
+          <div className="text-gray-900 font-medium text-lg">$ {price}</div>
+          <button
+            className="text-red-500 mt-2"
+            onClick={() => dispatch(removeItem(id))}
+          >
+            Remove
+          </button>
+        </div>
       </div>
     </div>
   );
