20a98e2d1ed6902b00a4fbc76ba0e3d0429078e1
Yahya <yahyahussein670@gmail.com>
2024-06-01 12:39:48 +0300

    fix: add react persist which stores cart item to local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,49 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../slices/ProductSlice";
 import { cartReducer } from "../slices/CartSlice";
+import categoryReducer from "../slices/CategorySlice";
+
+// export const store = configureStore({
+//   reducer: {
+//     products: productReducer,
+//     cart: cartReducer,
+//     categories: categoryReducer,
+//   },
+// });
+
+
+import { configureStore } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, cartReducer)
 
 export const store = configureStore({
   reducer: {
-    products: productReducer,
-    cart: cartReducer,
+     products: productReducer,
+     cart: persistedReducer,
+     categories: categoryReducer,
   },
-});
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
+})
+
+export const persistor = persistStore(store)
