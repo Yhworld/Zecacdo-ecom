30f971134a7970501681cf7cfe0f3892297c83aa
Yahya <yahyahussein670@gmail.com>
2024-08-09 19:47:02 +0300

    update: about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,30 +7,20 @@ let aboutImg = require("../../assets/campaign-creators-gMsnXqILjp4-unsplash.jpg"
 function About() {
   return (
     <>
-    <div id="about" className="container mx-auto p-4">
+    <div id="about" className="max-w-screen-xl container mx-auto p-4">
       <div className="flex flex-col lg:flex-row justify-between gap-8 mt-8">
         <div className="w-full lg:w-5/12 flex flex-col justify-center">
           <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">
             Our Story
           </h1>
           <p className="font-normal text-base leading-6 text-gray-600">
-            Founded in 2023, AV-System is a leading provider of innovative
-            audio, video, and networking solutions. Our founder, Ibrahim
-            Bahloul, envisioned a company that prioritizes innovation and
-            creativity in every project.
+          Our journey at Zecado began with a vision to revolutionize the perfume industry by offering a fresh perspective on fragrances. While traditional niche and designer fragrances have dominated the market for over 50 years, we set out to carve our own path as a niche fragrance brand with a focus on crafting unique scents that tell a story of individuality and style.
             <p className="pt-4 pb-4">
-              With a focus on user satisfaction, AV-System delivers tailored
-              solutions to meet the unique needs of each client. Our team of
-              skilled professionals is dedicated to creating immersive
-              audiovisual experiences that exceed expectations.
+            Inspired by my upbringing in a small town surrounded by nature's beauty – from serene lakes and beaches to the changing seasons – We here at Zecado developed a deep appreciation for the scents of the world around. These scents became a part of our most cherished memories, evoking feelings of comfort, joy, and nostalgia. It was during these moments that I discovered the transformative power of fragrances. The right scent had the ability to lift my mood, transport us to a different time and place, and even provide therapeutic benefits through its carefully curated ingredients.
             </p>
-            <p>
-              From designing cutting-edge audiovisual systems to implementing
-              reliable networking solutions, AV-System is committed to
-              delivering exceptional quality and service. We pride ourselves on
-              our commitment to innovation, reliability, and customer
-              satisfaction, making us your trusted partner in technology.
-            </p>
+            {/* <p>
+              
+            </p> */}
           </p>
         </div>
         <div className="">
@@ -57,7 +47,7 @@ function About() {
             Our Mission
           </h1>
           <p className="font-normal text-base leading-6 text-gray-600">
-          At AV-System, our mission is simple: to provide top-quality audio, video, and networking solutions that exceed expectations and enhance user experiences. With a commitment to innovation and customer satisfaction, we aim to deliver cutting-edge technology and personalized service to every client. Our goal is to revolutionize the industry by offering reliable, user-friendly solutions that empower our clients to thrive in today's digital landscape.
+          Rooted in the belief that confidence begins with the way you smell, our slogan, "Zecado, The Place Where Your Confidence Begins," embodies our mission to empower you through the power of scent. We believe that the right fragrance has the ability to evoke a sense of confidence that radiates from within, leaving a lasting impression on those around you. Our fragrances not only make you feel good but also have the potential to transport you to moments of nostalgia, reminding you of cherished memories and experiences.
           </p>
         </div>
       </div>
