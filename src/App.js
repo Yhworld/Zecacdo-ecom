faafb834871da4900efdc0884bae5113a5d5687c
Yahya <yahyahussein670@gmail.com>
2024-09-08 08:17:28 +0300

    fix: minor tweaks

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import Checkout from './components/checkout/Checkout';
 import OrderConfirmation from './components/order/OrderConfirmation';
 import About from './components/about/About';
 import PaymentInformation from './components/paymentinfo/PaymentInfo';
+import Confirmation from './components/confirmation/Confirmation';
+import Privacy from './components/Legal/Privacy';
+import Terms from './components/Legal/Terms';
 
 function App() {
   return (
@@ -23,11 +26,13 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Signin />} />
           <Route path="/shop" element={<Shop />} />
-          {/* <Route path="/payment" element={<PaymentInformation />} /> */}
+          <Route path="/return" element={<Confirmation/>} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/register" element={<Signup />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/product/:id" element={<Product />} />
+          <Route path="/privacy-policy" element={<Privacy />} />
+          <Route path="/terms-and-conditions" element={<Terms />} />
           <Route path="/orderid/:orderId" element={<OrderConfirmation />} />
         </Routes>
       </div>
