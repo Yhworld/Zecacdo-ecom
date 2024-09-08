77d99fc1fb5d5084fd1911407a20924eb56210cb
Yahya <yahyahussein670@gmail.com>
2024-09-08 08:14:38 +0300

    create: terms and privacy components

---
+++ src/components/Legal/Privacy.js
@@ -0,0 +1,112 @@
+import React from "react";
+
+function Privacy() {
+  return (
+    <div className="max-w-screen-xl container mx-auto p-8 mt-6 flex flex-col items-center ">
+      <div className="pl-4 space-y-4 md:w-3/4 ">
+        <h1 className="text-3xl font-bold pb-2">
+          Privacy Policy for Zecado LLC
+        </h1>
+        <p className="pb-4 leading-8">
+          Welcome to Zecado LLC ("we," "us," "our"). This Privacy Policy
+          explains how we collect, use, disclose, and safeguard your information
+          when you visit our website, ZecadoFragrance.com (the "Site"), and when
+          you make a purchase from us. We value your privacy and are committed
+          to protecting your personal information.
+        </p>
+        <h1 className="text-2xl font-semibold">1. Information We Collect</h1>
+        <p className="leading-10 pb-12">
+          When you visit our Site and make a purchase, we may collect the
+          following types of information:
+          <br />
+          <strong>Personal Information:</strong> This includes your name, email
+          address, shipping address, billing address, phone number, and payment
+          information (credit card, debit card, PayPal, Apple Pay, etc.).
+        </p>
+        <h1 className="text-2xl font-semibold">
+          2. How We Use Your Information
+        </h1>
+        <p className="leading-10 pb-12">
+          We use the information we collect for the following purposes:
+          <br />
+          <strong>Processing Transactions:</strong> To process your purchases,
+          manage your orders, and deliver the products you have ordered.
+          <br />
+          <strong>Customer Support:</strong> To provide customer support and
+          respond to your inquiries.
+          <br />
+          <strong>Improving Our Services: </strong>To understand how our
+          customers use our Site and to enhance our services and product
+          offerings.
+          <br />
+          <strong>Communications:</strong> If you sign up for it, to send you
+          updates, promotional materials, and information about upcoming
+          products as a form of marketing to keep you up-to-date. You can
+          opt-out of receiving promotional communications at any time.
+        </p>
+        <h1 className="text-2xl font-semibold">
+        3. Confidentiality and Security
+        </h1>
+        <p className="leading-10 pb-12">
+        We value the confidentiality and security of your personal information. To ensure this:
+          <br />
+          <strong>Confidentiality:</strong> We will not share, sell, or rent your personal information to third parties without your consent, except as required by law or as necessary to process your order.
+          <br />
+          <strong>Data Security:</strong> We implement appropriate technical and organizational measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction.
+          <br />
+          <strong>Payment Information:</strong> We do not store credit card or debit card information on file. Payment information is used solely for processing your transaction and is not retained.
+        </p>
+        <h1 className="text-2xl font-semibold">
+        4. Data Retention
+        </h1>
+        <p className="leading-10 pb-12">
+        We retain your personal information for as long as necessary to fulfill the purposes outlined in this Privacy Policy or as required by law. Your purchase history may be retained for record-keeping purposes.
+        </p>
+        <h1 className="text-2xl font-semibold">
+        5. Your Rights
+        </h1>
+        <p className="leading-10 pb-12">
+        You have the right to:
+          <br />
+          <strong>Access and Update: </strong> Access and update your personal information by contacting us.
+          <br />
+          <strong>Opt-Out: </strong>  Opt-out of receiving promotional communications from us by following the unsubscribe instructions in the communication or by contacting us directly.
+          <br />
+          <strong>Data Deletion: </strong> Request the deletion of your personal information, subject to our need to retain certain information for legal or administrative purposes.
+        </p>
+        <h1 className="text-2xl font-semibold">
+        6. Refund Policy
+        </h1>
+        <p className="leading-10 pb-12">
+        We offer a refund option for purchases made within 48 hours after receiving the product, provided there is no damage, contamination, or tampering with the package and product. Please contact our customer service team for assistance with refunds.
+        </p>
+        <h1 className="text-2xl font-semibold">
+        7. Changes to This Privacy Policy
+        </h1>
+        <p className="leading-10 pb-12">
+        We may update this Privacy Policy from time to time. Any changes will be posted on this page with an updated effective date. We encourage you to review this Privacy Policy periodically to stay informed about how we are protecting your information.
+        </p>
+        <h1 className="text-2xl font-semibold">
+        8. Contact Us
+        </h1>
+        <p className="leading-10 pb-12">
+        If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at:
+        <br/>
+        Zecado LLC  
+        <br/>
+        <a className="text-blue-600" href="mailto:Info@Zecadofragrance.com">Info@Zecadofragrance.com</a>
+        <br/>
+        +1 (207) 891 - 1759
+        <br/>
+        By using our Site and services, you acknowledge that you have read and understood this Privacy Policy and agree to its terms. Thank you for trusting Zecado LLC with your personal information.
+        <br/>
+        <br/>
+        <div className="font-semibold">Zecado– The Place Where Your Confidence Begins.</div>
+        </p>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Privacy;
---
+++ src/components/Legal/Terms.js
@@ -0,0 +1,230 @@
+import React from "react";
+
+function Terms() {
+  return (
+    <div className="max-w-screen-xl container mx-auto p-8 mt-6 flex flex-col items-center ">
+      <div className="pl-4 space-y-4 md:w-3/4 ">
+        <h1 className="text-3xl font-bold pb-2">
+        Zecado Terms and Conditions
+        </h1>
+        <p className="pb-4 leading-8">
+        Welcome to Zecado LLC ("we," "us," "our"). By accessing and using our
+website, ZecadoFragrance.com (the "Site"), you agree to be bound by the
+following terms and conditions ("Terms"). Please read them carefully before
+using our Site or making any purchases.
+        </p>
+        <h1 className="text-2xl font-semibold">1. Introduction</h1>
+        <p className="leading-10 pb-12">
+        This website (Site) is operated by Zecado LLC. By using the Site, you agree
+to these Terms and our Privacy Policy.
+          <br />
+          <br/>
+          <h1 className="text-2xl font-semibold">2. Use of the Site, Account and Orders:</h1>
+          <br/>
+          (a) By placing an order on the Site, you agree to these Terms.
+          <br/>
+          (b) To be eligible to purchase products on the Site, you must be at least 18
+years old or at least 16 years old with the consent and supervision of a
+guardian.
+<br/>
+          (c) You can buy products with or without an account
+          <br/>
+          (d) Ensure your personal information on your account is accurate and
+up-to-date. We handle your personal information in accordance with our Privacy Policy. Keep your account details private as you will be responsible
+for all activity and purchases made with your account.
+<br/>
+(e) You can order products from us through the Site. Placing an order
+means you are purchasing the product(s) at the listed price,
+including delivery fees and taxes.
+<br />
+(f) Review order details (products, delivery, pricing) before submitting your
+order. After you place an order, you will receive an order confirmation email
+with details of your order.
+<br/>
+(g) Purchases on the Site are subject to availability. We strive to keep
+products in stock and the Site updated.
+<br/>
+(h) If a product is available for pre-order only, this will be clearly indicated
+on our Site.
+<br/>
+(i) We may cancel, at any time before delivery and for whatever reason, an
+order that we have previously accepted, including where there is a
+considerable delay in dispatching your order, if for any reason we cannot
+supply the products you order (for example, for an event beyond our
+reasonable control) or if products ordered were subject to an error on our
+Site (for example, in relation to a description, price or image). We will
+contact you using the details you provided when you placed your order.
+
+        </p>
+        <h1 className="text-2xl font-semibold">
+        3. Products
+        </h1>
+        <p className="leading-10 pb-12">
+        (a) We have made every effort to ensure that all product details,
+descriptions, and images on the Site are correct at the time the information
+was entered into the system and as accurate as possible with the products
+that appear in-store.
+          <br />
+          (b) All prices for our products on the Site are in the currency set out in the
+listing and any taxes will be set out separately, if applicable.
+          <br />
+          (c) The products listed on the Site are meant for personal use only. You
+must not sell or resell any of these products. We reserve the right, with or
+without notice, to cancel or reduce the quantity of any products in an order
+that may result in the violation of these Terms.
+          <br />
+          (d) We reserve the right to limit the sales of our products to any person,
+geographic region or jurisdiction and this may be exercised on a
+case-by-case basis.
+         
+        </p>
+        <h1 className="text-2xl font-semibold">
+        4. Price and Payments
+        </h1>
+        <p className="leading-10 pb-12">
+        (a) You must pay us the purchase price of each product you order upfront,
+including any applicable delivery costs as set out in our 'Shipping, Delivery
+& Returns' Policy.
+          <br />
+          (b) You must not pay, or attempt to pay, the Price by fraudulent or unlawful
+means. If you make a payment by debit card or credit card, you warrant that
+you are authorized to use the debit card or credit card to make the payment.
+          <br />
+          (c) Payment methods include Apple Pay, credit cards, and debit cards. We
+are not a third-party website; we process all transactions through our Site.
+          <br />
+          (d) We do not store any credit card details. All payment information is
+collected and stored through our secure payment processors.
+<br/>
+(e) For international deliveries, you may need to pay customs, charges, or
+taxes in addition to the Price.
+<br/>
+(f) We may from time to time issue promotional discount codes for certain
+products on the Site. To claim the discount, you must enter the promotional
+discount code at the time of submitting your order through the Site.
+<br/>
+(g) The terms for using promotional codes and participating in
+competitions will be listed on the Site.
+        </p>
+        <h1 className="text-2xl font-semibold">
+        5. Shipping, Delivery and Returns
+        </h1>
+        <p className="leading-10 pb-12">
+        (a) Please refer to our 'Shipping, Delivery & Returns' Policy on our Site for
+detailed information.
+<br/>
+(b) Nothing in these Terms attempts to modify or exclude the conditions,
+warranties, and undertakings, and other legal rights, under the applicable
+consumer law
+        </p>
+        <h1 className="text-2xl font-semibold">
+        6. Limitations
+        </h1>
+        <p className="leading-10 pb-12">
+        (a) Despite anything to the contrary, but subject to your rights under
+consumer law, to the maximum extent permitted by law:
+          <br />
+          (b) Neither Party will be liable for Consequential Loss;
+          <br />
+          (c) Each Party’s liability under these Terms will be reduced proportionately
+based on the other Party’s acts or omissions; and
+          <br />
+          (c) Our aggregate liability for any Liability arising from or in connection
+with the Terms will be limited to the Price you pay us for the products.
+        </p>
+        <h1 className="text-2xl font-semibold">
+        7. General
+        </h1>
+        <p className="leading-10 pb-12">
+        (a) <strong>Disputes:</strong> Before taking legal action, both Parties agree to meet and
+attempt to resolve disputes in good faith. If unresolved, the Parties agree to
+engage a mediator to attempt to resolve the dispute. The costs of the
+mediation will be shared equally between the Parties. Nothing in this
+clause will operate to prevent a Party from seeking urgent injunctive or
+equitable relief from a court of appropriate jurisdiction.
+<br/>
+(b) <strong>Notices:</strong> Written notices must be sent to the provided details via
+standard post or email. Notice is deemed served 48 hours after posting or
+upon email transmission.
+<br/>
+(c)<strong>Force Majeure:</strong>  Neither Party will be liable for any delay or failure due to
+a Force Majeure Event, as long as the Party notified the other Party as soon
+as reasonably practicable in writing about the Force Majeure Event and has
+made reasonable efforts to minimize the impact.
+<br/>
+(d)<strong>Feedback and complaints:</strong>  We are always looking to improve our
+services. If you have any feedback or a complaint, please contact us and we
+will take reasonable steps to address any concerns you have.
+<br/>
+(e) <strong> Assignment:</strong> You must not assign any rights or obligations under these
+Terms, whether in whole or in part, without our prior written consent.
+<br/>
+(f) <strong>Amendment:</strong> We may, at any time and at our discretion, update these
+Terms by publishing them on the Site. Before you place an order, please
+carefully read the terms to ensure you understand and agree to them. Upon
+our acceptance of an order, the terms and conditions that apply will be the
+ones that were in effect (and which you agreed to) when you placed your
+order.
+<br/>
+(g) <strong>Governing law:</strong> These Terms are governed by the laws of [Your
+Jurisdiction], and both parties submit to the exclusive jurisdiction of the
+courts in [Your Jurisdiction].
+<br/>
+(h) <strong>Third-party sites: </strong>The Site may contain links to websites operated by
+third parties. Unless we tell you otherwise, we do not control, endorse, or
+approve, and are not responsible for, the content on those websites. We
+recommend that you make your own investigations with respect to the
+suitability of those websites. If you purchase goods or services from a
+third-party website linked from the Site, such third party provides the
+goods and services to you, not us. We may receive a benefit (which may
+include a referral fee or a commission) should you visit certain third-party
+websites via a link on the Site (Affiliate Link) or for featuring certain
+products or services on the Site. We will make it clear by notice to you
+which (if any) products or services we receive a benefit to feature on the
+Site, or which (if any) third-party links are Affiliate Links
+
+        </p>
+        <h1 className="text-2xl font-semibold">
+        8. Definitions
+        </h1>
+        <p className="leading-10 pb-12">
+        (a) Consequential Loss means any unexpected loss or damage resulting
+from a breach, act, or omission, regardless of whether it was foreseeable at
+the time of entering into these Terms. This includes any real or anticipated
+loss of profit, benefit, revenue, business, goodwill, opportunity, savings,
+reputation, use, or data. You agree that your obligation to pay us the Price
+under these Terms will not constitute “Consequential Loss”.
+<br/>
+(b) Force Majeure Event means any event or circumstance which is beyond
+a Party’s reasonable control.
+<br/>
+(c) Liability means any expense, cost, liability, loss, damage, claim, notice,
+entitlement, investigation, demand, proceeding or judgment (whether
+under statute, contract, equity, tort (including negligence), indemnity or
+otherwise), howsoever arising, whether direct or indirect and/or whether
+present, unascertained, future or contingent and whether involving a third
+party or a party to these Terms or otherwise.
+<br/>
+<br/>
+By using our Site and services, you acknowledge that you have read and
+understood these Terms and agree to abide by them. Thank you for
+choosing Zecado. If you have any questions or concerns about this Terms
+and condition please contact us at:
+<br/>
+<br/>
+        Zecado LLC  
+        <br/>
+        <a className="text-blue-600" href="mailto:Info@Zecadofragrance.com">Info@Zecadofragrance.com</a>
+        <br/>
+        +1 (207) 891 - 1759
+        <br/>
+        <br/>
+        <div className="font-semibold">Zecado– The Place Where Your Confidence Begins.</div>
+        </p>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Terms;
